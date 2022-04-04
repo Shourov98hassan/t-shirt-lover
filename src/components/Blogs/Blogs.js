abbf027d0d2a767d90a3e8fa0909cfda1ec7f143
Shourov98hassan <kamruzzamanshourov98@gmail.com>
2022-04-04 08:34:24 -0700

    second question

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -6,7 +6,7 @@ const Blogs = () => {
            <h3>What is Context API?</h3>
            <p><span style={{color:'red'}} >Answer:</span> The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" <br />
             or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
-
+            <h3>What is Semantic Tag?</h3>
         </div>
     );
 };
