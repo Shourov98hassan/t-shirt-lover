5e23054c1d131f6f31e8b761561125d3c0893c7c
Shourov98hassan <kamruzzamanshourov98@gmail.com>
2022-04-04 08:35:17 -0700

    second question answer

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -7,6 +7,9 @@ const Blogs = () => {
            <p><span style={{color:'red'}} >Answer:</span> The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" <br />
             or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
             <h3>What is Semantic Tag?</h3>
+            <p><span style={{color:'red'}}>Answer:</span> Semantic HTML tags provide information about the contents of those tags that goes beyond just how they look on a page. Text that is enclosed in the  tag <br />
+             is immediately recognized by the browser as some type of coding language.</p>
+
         </div>
     );
 };
