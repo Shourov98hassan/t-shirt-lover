646dc85b058b0ff9e9f4eab0bc5beaeda40e40d9
Shourov98hassan <kamruzzamanshourov98@gmail.com>
2022-04-04 08:37:12 -0700

    adding a paragraph in about component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,6 +4,8 @@ const About = () => {
     return (
         <div>
             <h1>this is about</h1>
+            <p>what kinds of data do you wants from me?</p>
+        
         </div>
     );
 };
